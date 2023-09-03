670178f1b6fbefbc424520a7d39a588a77f1c1d8
miloszoleksinski <miloszoleksinski@gmail.com>
2023-09-03 19:54:59 +0200

    Add ConfigProperties that will fetch selected algorithm

---
+++ anomaly-detector/src/main/java/io/kontak/apps/anomaly/detector/config/ConfigProperties.java
@@ -0,0 +1,18 @@
+package io.kontak.apps.anomaly.detector.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "anomaly")
+public class ConfigProperties {
+    private String algorithm;
+
+    public String getAlgorithm() {
+        return algorithm;
+    }
+
+    public void setAlgorithm(String algorithm) {
+        this.algorithm = algorithm;
+    }
+}
