acc1185c2a7198e9536f3bc9cd8ed7ba078dffae
miloszoleksinski <miloszoleksinski@gmail.com>
2023-09-03 20:02:29 +0200

    Fix algorithms to match to streaming idea (processing one by one)

--- anomaly-detector/src/main/java/io/kontak/apps/anomaly/detector/AnomalyDetectorAlgorithmOne.java
+++ anomaly-detector/src/main/java/io/kontak/apps/anomaly/detector/AnomalyDetectorAlgorithmOne.java
@@ -2,37 +2,50 @@ package io.kontak.apps.anomaly.detector;
 
 import io.kontak.apps.event.Anomaly;
 import io.kontak.apps.event.TemperatureReading;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.List;
 import java.util.Optional;
 
+@Component("anomalyDetectorAlgorithmOne")
 public class AnomalyDetectorAlgorithmOne implements AnomalyDetector {
 
     private static final double ANOMALY_THRESHOLD = 5.0;
+    private static final int WINDOW_SIZE = 10;
+    private final Deque<TemperatureReading> readingBuffer = new ArrayDeque<>(WINDOW_SIZE);
 
     @Override
     public Optional<Anomaly> apply(List<TemperatureReading> temperatureReadings) {
-        int numberOfReadings = temperatureReadings.size();
-        for(int i=0; i < numberOfReadings; i++) {
-            if (i + 10 <= numberOfReadings) {
-                List<TemperatureReading> readings = temperatureReadings.subList(i, i + 10);
-                boolean isAnomaly = isAnomaly(readings);
-                if (isAnomaly) {
-                    return Optional.of(new Anomaly(readings.get(9)));
+        Optional<Anomaly> anomaly = Optional.empty();
+
+        for (TemperatureReading reading : temperatureReadings) {
+            readingBuffer.addLast(reading);
+
+            while (readingBuffer.size() > WINDOW_SIZE) {
+                readingBuffer.removeFirst();
+            }
+
+            if (readingBuffer.size() == WINDOW_SIZE) {
+                System.out.println("reading bufer size equals windows size");
+                if (isAnomaly(readingBuffer)) {
+                    System.out.println("we have found an anomaly, temperature: " + reading.temperature() );
+                    anomaly = Optional.of(new Anomaly(reading));
                 }
             }
         }
-        return Optional.empty();
+        return anomaly;
     }
 
-    private boolean isAnomaly(final List<TemperatureReading> readings) {
+    private boolean isAnomaly(final Deque<TemperatureReading> readings) {
         double sumOfFirstNineTemperatures = sumOfFirstNine(readings);
         double averageOfFirstNineTemperatures = sumOfFirstNineTemperatures / 9;
-        TemperatureReading currentReading = readings.get(9);
+        TemperatureReading currentReading = readings.getLast();
         return currentReading.temperature() > averageOfFirstNineTemperatures + ANOMALY_THRESHOLD;
     }
 
-    private double sumOfFirstNine(List<TemperatureReading> readings) {
-        return readings.subList(0, 9).stream().mapToDouble(TemperatureReading::temperature).sum();
+    private double sumOfFirstNine(final Deque<TemperatureReading> readings) {
+        return readings.stream().limit(9).mapToDouble(TemperatureReading::temperature).sum();
     }
 }
--- anomaly-detector/src/main/java/io/kontak/apps/anomaly/detector/AnomalyDetectorAlgorithmTwo.java
+++ anomaly-detector/src/main/java/io/kontak/apps/anomaly/detector/AnomalyDetectorAlgorithmTwo.java
@@ -2,44 +2,55 @@ package io.kontak.apps.anomaly.detector;
 
 import io.kontak.apps.event.Anomaly;
 import io.kontak.apps.event.TemperatureReading;
+import org.springframework.stereotype.Component;
 
 import java.time.Instant;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.List;
 import java.util.Optional;
 
+@Component("anomalyDetectorAlgorithmTwo")
 public class AnomalyDetectorAlgorithmTwo implements AnomalyDetector {
 
     private static final double ANOMALY_THRESHOLD = 5.0;
+    private static final int WINDOW_SIZE_SECONDS = 10;
+    private final Deque<TemperatureReading> readingBuffer = new ArrayDeque<>();
 
     @Override
     public Optional<Anomaly> apply(List<TemperatureReading> temperatureReadings) {
-        int numberOfReadings = temperatureReadings.size();
-        for (int i = 0; i < numberOfReadings; i++) {
-            if (i + 10 <= numberOfReadings) {
-                List<TemperatureReading> consecutiveReadings = temperatureReadings.subList(i, i + 10);
-                if (isWithinTimeWindow(consecutiveReadings) && isAnomaly(consecutiveReadings)) {
-                    return Optional.of(new Anomaly(consecutiveReadings.get(9)));
+        Optional<Anomaly> anomaly = Optional.empty();
+
+        for (TemperatureReading reading : temperatureReadings) {
+            readingBuffer.addLast(reading);
+
+            while (!readingBuffer.isEmpty() && !isWithinTimeWindow(readingBuffer.getFirst(), reading.timestamp())) {
+                readingBuffer.removeFirst();
+            }
+
+            if (readingBuffer.size() >= 2) { // Minimum 2 readings needed to compare
+                double averageTemperature = calculateAverageTemperature(readingBuffer);
+                double currentTemperature = reading.temperature();
+                if (isAnomaly(currentTemperature, averageTemperature)) {
+                    anomaly = Optional.of(new Anomaly(reading));
                 }
             }
         }
-        return Optional.empty();
+        return anomaly;
     }
 
-    private boolean isWithinTimeWindow(List<TemperatureReading> readings) {
-        Instant currentTimestamp = readings.get(9).timestamp();
-        Instant oldestTimestamp = readings.get(0).timestamp();
-        long timeDifferenceSeconds = currentTimestamp.minusSeconds(oldestTimestamp.getEpochSecond()).getEpochSecond();
-        return timeDifferenceSeconds <= 10;
+    private boolean isWithinTimeWindow(final TemperatureReading reading, final Instant currentTime) {
+        Instant readingTime = reading.timestamp();
+        long timeDifferenceSeconds = Math.abs(readingTime.getEpochSecond() - currentTime.getEpochSecond());
+        return timeDifferenceSeconds <= WINDOW_SIZE_SECONDS;
     }
 
-    private boolean isAnomaly(List<TemperatureReading> readings) {
-        double averageTemperature = calculateAverageTemperature(readings);
-        TemperatureReading currentReading = readings.get(9);
-        return Math.abs(currentReading.temperature() - averageTemperature) > ANOMALY_THRESHOLD;
-    }
-
-    private double calculateAverageTemperature(List<TemperatureReading> readings) {
+    private double calculateAverageTemperature(final Deque<TemperatureReading> readings) {
         double sumOfTemperatures = readings.stream().mapToDouble(TemperatureReading::temperature).sum();
         return sumOfTemperatures / readings.size();
+    }
+
+    private boolean isAnomaly(final double currentTemperature, final double averageTemperature) {
+        return Math.abs(currentTemperature - averageTemperature) > ANOMALY_THRESHOLD;
     }
 }
